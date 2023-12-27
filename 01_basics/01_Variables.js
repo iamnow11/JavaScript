7325a46ded96839343ec869a6909cad1a0365f18
Abhishek Shah <61278322+iamnow11@users.noreply.github.com>
2023-12-27 20:51:39 +0000

    datatype

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountID = 1223345
+let accountEmail = "Abhi@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+accountEmail = "hfgf@gmail.com"
+accountCity = "Ahmedabad"
+accountPassword = "53535"
+let accountState;
+
+/*
+Prefer not to use var 
+*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // use all js code as new version
+
+// alert(3+3) we are using node not using browser
+
+let Name = "Abhishek"
+let age = 25
+let isLoggedIn = true 
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefiend => when you create variable but not assign the value means undefiend.
+//symbol=> Unique
+
+console.log(typeof age);
+console.log(typeof null);
