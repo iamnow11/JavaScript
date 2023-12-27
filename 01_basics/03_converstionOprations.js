856e8fad2f18e36634480c3452a8a61c78ab30aa
Abhishek Shah <61278322+iamnow11@users.noreply.github.com>
2023-12-27 21:16:02 +0000

    Converstion of datatype

---
+++ 01_basics/03_converstionOprations.js
@@ -0,0 +1,25 @@
+let score = "55abc"
+
+//console.log(typeof score);
+
+let inValue = Number(score)
+//console.log(typeof inValue );
+//console.log(inValue);
+
+// "33" => 33
+// "33abc"  => NaN (Not a number)
+// ture => 1 ; false => 0
+
+let Inloggedin = "Abhishek"
+let booleanIsloggedin = Boolean(Inloggedin)
+
+//console.log(booleanIsloggedin);
+
+// 1 => true
+//"" => false
+//"Abhishek" => true
+
+let someNumber = 33
+let inSomenumber = String(someNumber)
+console.log(typeof inSomenumber);
+console.log(someNumber);
