9d2ded802412d176950dcb892e74287104991e14
Abhishek Shah <61278322+iamnow11@users.noreply.github.com>
2024-01-05 20:58:37 +0000

    datatyoe summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,13 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefiend, symbol, BigInt
+
+// Refrfence (Non Primitive)
+const score = 100;
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsidetemp = null
+
+const anotherID = Symbol('123')
+console.log();
