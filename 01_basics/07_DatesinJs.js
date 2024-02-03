34d0407ec65d5c1e05ad43d74472ea855ef9bdd1
Abhishek Shah <61278322+iamnow11@users.noreply.github.com>
2024-02-03 16:46:16 +0000

    Date

---
+++ 01_basics/07_DatesinJs.js
@@ -0,0 +1,8 @@
+ let myDate = new Date()
+//  console.log(myDate.toISOString());
+
+ let myCreatedate = new Date(2023, 1, 20)
+//  console.log(myCreatedate.toDateString());
+
+let myTimestamp = Date.now()
+console.log(myTimestamp);
